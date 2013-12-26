b59d0c936ac21968f524efbf58c683ab629cca6d
Slavi Pantaleev <s.pantaleev@gmail.com>
2013-12-26 23:02:57 +0200

    Initial commit

---
+++ src/Devture/Component/DBAL/Model/BaseModel.php
@@ -0,0 +1,32 @@
+<?php
+namespace Devture\Component\DBAL\Model;
+
+abstract class BaseModel {
+
+	private $record = array();
+
+	public function __construct(array $record = array()) {
+		$this->record = $record;
+	}
+
+	protected function getAttribute($key, $defaultValue = null) {
+		return (isset($this->record[$key]) || array_key_exists($key, $this->record) ? $this->record[$key] : $defaultValue);
+	}
+
+	protected function setAttribute($key, $value) {
+		$this->record[$key] = $value;
+	}
+
+	public function export() {
+		return $this->record;
+	}
+
+	public function getId() {
+		return $this->getAttribute('_id', null);
+	}
+
+	public function setId($value) {
+		$this->setAttribute('_id', $value);
+	}
+
+}
